5ca6da37884211931bac55a9906c90538fdab5a8
Andrei <zoitber9@mail.ru>
2023-06-28 23:29:12 +0300

    refactor: prettier

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,22 +1,22 @@
-import { tasksReducer } from 'features/TodolistsList/tasks.reducer';
-import { todolistsReducer } from 'features/TodolistsList/todolists.reducer';
-import { AnyAction, combineReducers } from 'redux'
-import { ThunkAction, ThunkDispatch } from 'redux-thunk'
-import { appReducer } from 'app/app.reducer'
-import { authReducer } from 'features/auth/auth.reducer'
-import { configureStore } from '@reduxjs/toolkit';
+import { tasksReducer } from "features/TodolistsList/tasks.reducer";
+import { todolistsReducer } from "features/TodolistsList/todolists.reducer";
+import { AnyAction, combineReducers } from "redux";
+import { ThunkAction, ThunkDispatch } from "redux-thunk";
+import { appReducer } from "app/app.reducer";
+import { authReducer } from "features/auth/auth.reducer";
+import { configureStore } from "@reduxjs/toolkit";
 
 
 const rootReducer = combineReducers({
-	tasks: tasksReducer,
-	todolists: todolistsReducer,
-	app: appReducer,
-	auth: authReducer
-})
+  tasks: tasksReducer,
+  todolists: todolistsReducer,
+  app: appReducer,
+  auth: authReducer
+});
 
 export const store = configureStore({
-	reducer: rootReducer,
-})
+  reducer: rootReducer
+});
 
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
