23150f5e85c8284b074c59321a5a32d41b841c5b
Andrei <zoitber9@mail.ru>
2023-08-23 11:50:26 +0300

    add custom hook and structure refactoring

--- src/features/TodolistsList/todolists.reducer.test.ts
+++ src/features/TodolistsList/todolists.reducer.test.ts
@@ -3,10 +3,11 @@ import {
   TodolistDomainType,
   todolistsActions,
   todolistsReducer,
+  todolistsThunks,
 } from "features/TodolistsList/todolists.reducer";
 import { v1 } from "uuid";
-import { TodolistType } from "api/todolists-api";
 import { RequestStatusType } from "app/app.reducer";
+import { TodolistType } from "./todolists.api";
 
 let todolistId1: string;
 let todolistId2: string;
@@ -16,29 +17,15 @@ beforeEach(() => {
   todolistId1 = v1();
   todolistId2 = v1();
   startState = [
-    {
-      id: todolistId1,
-      title: "What to learn",
-      filter: "all",
-      entityStatus: "idle",
-      addedDate: "",
-      order: 0,
-    },
-    {
-      id: todolistId2,
-      title: "What to buy",
-      filter: "all",
-      entityStatus: "idle",
-      addedDate: "",
-      order: 0,
-    },
+    { id: todolistId1, title: "What to learn", filter: "all", entityStatus: "idle", addedDate: "", order: 0 },
+    { id: todolistId2, title: "What to buy", filter: "all", entityStatus: "idle", addedDate: "", order: 0 },
   ];
 });
 
 test("correct todolist should be removed", () => {
   const endState = todolistsReducer(
     startState,
-    todolistsActions.removeTodolist({ id: todolistId1 })
+    todolistsThunks.removeTodolist.fulfilled({ id: todolistId1 }, "requestId", todolistId1),
   );
 
   expect(endState.length).toBe(1);
@@ -53,7 +40,10 @@ test("correct todolist should be added", () => {
     order: 0,
   };
 
-  const endState = todolistsReducer(startState, todolistsActions.addTodolist({ todolist }));
+  const endState = todolistsReducer(
+    startState,
+    todolistsThunks.addTodolist.fulfilled({ todolist }, "requestId", todolist.title),
+  );
 
   expect(endState.length).toBe(3);
   expect(endState[0].title).toBe(todolist.title);
@@ -63,7 +53,9 @@ test("correct todolist should be added", () => {
 test("correct todolist should change its name", () => {
   let newTodolistTitle = "New Todolist";
 
-  const action = todolistsActions.changeTodolistTitle({ id: todolistId2, title: newTodolistTitle });
+  const args = { id: todolistId2, title: newTodolistTitle };
+
+  const action = todolistsThunks.changeTodolistTitle.fulfilled(args, "requestId", args);
 
   const endState = todolistsReducer(startState, action);
 
@@ -81,26 +73,20 @@ test("correct filter of todolist should be changed", () => {
   expect(endState[0].filter).toBe("all");
   expect(endState[1].filter).toBe(newFilter);
 });
+
 test("todolists should be added", () => {
-  const action = todolistsActions.setTodolists({ todolists: startState });
-
+  const action = todolistsThunks.fetchTodolists.fulfilled({ todolists: startState }, "requestId");
   const endState = todolistsReducer([], action);
-
   expect(endState.length).toBe(2);
 });
+
 test("correct entity status of todolist should be changed", () => {
   let newStatus: RequestStatusType = "loading";
 
-  const action = todolistsActions.changeTodolistEntityStatus({
-    id: todolistId2,
-    entityStatus: newStatus,
-  });
+  const action = todolistsActions.changeTodolistEntityStatus({ id: todolistId2, entityStatus: newStatus });
 
   const endState = todolistsReducer(startState, action);
 
   expect(endState[0].entityStatus).toBe("idle");
   expect(endState[1].entityStatus).toBe(newStatus);
 });
-
-
-
